85c0e69c0cde99389dc23551049d71dc341fe50a
Armand Gurgu <armandgurgu23@gmail.com>
2020-09-04 22:00:27 -0400

    Added skeleton folder structure for scrapper.

---
+++ src/queryEngine/queryEngine.py
@@ -0,0 +1,19 @@
+import urllib
+from urllib import request
+
+
+class ArxivQueryEngine(object):
+    def __init__(self, maxResults, queriesFile):
+        self.maxResults = maxResults
+        self.queriesFile = queriesFile
+
+    def fetchSingleSearchQuery(self, queryString):
+        sampleUrl = 'http://export.arxiv.org/api/query?search_query=all:{}&start=0&max_results={}'.format(
+            queryString, self.maxResults)
+        return self.queryContentsFromUrl(sampleUrl)
+
+    def queryContentsFromUrl(self, urlString):
+        # May need to wrap this later in a try/except to handle
+        # connection errors.
+        urlContents = request.urlopen(urlString).read()
+        return urlContents.decode('utf-8')
---
+++ src/runScrapper.py
@@ -0,0 +1,7 @@
+
+def scrapperRunner():
+    pass
+
+
+if __name__ == "__main__":
+    scrapperRunner()
